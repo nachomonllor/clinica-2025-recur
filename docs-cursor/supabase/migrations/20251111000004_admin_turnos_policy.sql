01156a39bccede8b19567a66015631dbd9afb9c8
nachomonllor <nachomonllorc@gmail.com>
2025-11-12 22:58:01 -0300

    Remover documentación provisoria

--- docs-cursor/supabase/migrations/20251111000004_admin_turnos_policy.sql
+++
@@ -1,9 +0,0 @@
--- Permitir que los administradores puedan consultar todos los turnos
-CREATE POLICY "Admins can view all turnos" ON public.turnos
-  FOR SELECT USING (
-    EXISTS (
-      SELECT 1
-      FROM public.profiles
-      WHERE id = auth.uid() AND rol = 'admin'
-    )
-  );
