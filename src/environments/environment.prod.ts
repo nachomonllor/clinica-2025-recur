609b0d57f157d7e5336a9b472d7b4d6a391dab59
nachomonllor <nachomonllorc@gmail.com>
2025-12-01 19:54:40 -0300

    botones de acceso rapido [login] 6 usuarios: (3 pacientes, 2 especialistas, 1 admin)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,14 +7,10 @@ export const environment = {
   captchaEnabled: true,
   recaptchaSiteKey: 'SITE_KEY_DEL_CAPTCHA_ACA',
 
+ 
+
   quickLogins: {
     paciente: [
-      // {
-      //   email: 'maradona@hotmail.com',
-      //   password: '123456',
-      //   nombre: 'diego maradona',
-      //   avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/7781eb7a-8c24-4631-a63c-4b9739c3e149/1763943378241_1_diego.jpg'
-      // },
       {
         email: 'ana@hotmail.com',
         password: '123456',
@@ -38,13 +34,6 @@ export const environment = {
     ],
 
     especialista: [
-
-      {
-        email: 'gustavo@hotmail.com',
-        password: '123456',
-        nombre: 'gustavo ceratti',
-        avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/bf9cdbf0-d13f-4c14-99a7-56997e2dd447/1763934863767_1_ceratti.jpg'
-      },
       {
         email: 'alberto@hotmail.com',
         password: '123456',
@@ -52,56 +41,15 @@ export const environment = {
         avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/57f31c41-37d5-4f6b-8e69-1132eb0453dc/1763889873282_1_albert.jpg'
       },
       {
-        email: 'dexter@hotmail.com',
-        password: '123456',
-        nombre: 'dexter holland',
-        avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/73f3eb2a-9676-4c2b-9a2b-7b63d380c439/1764029033182_1_dexter_holland.jpg'
-      },
-      {
-        email: 'isaac@hotmail.com',
-        password: '123456',
-        nombre: 'isaac newton',
-        avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/76548ca7-0287-48ce-8b6b-669a7fe33c18/1763930864652_1_indio.jpg'
-      },
-      {
         email: 'nora@hotmail.com',
         password: '123456',
         nombre: 'Nora Da Puente',
         avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/35994f86-ef81-438f-b8f7-b27a35ea7902/1764020489484_1_julia.jpg'
       },
-      {
-        email: 'darwin@hotmail.com',
-        password: '123456',
-        nombre: 'charles darwin',
-        avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/86b2829d-2669-4ab9-b573-a7c9897d191e/1764046471863_1_darwin.jpg'
-      },
-      {
-        email: 'tesla@hotmail.com',
-        password: '123456',
-        nombre: 'nicola tesla',
-        avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/da846dbf-6019-49b2-ad7e-a687af3ba856/1764043232033_1_tesla.jpg'
-      },
-      {
-        email: 'galileo@hotmail.com',
-        password: '123456',
-        nombre: 'galileo galilei',
-        avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/96e33286-0f5d-4aca-8321-2f7f61ea367d/1764045087133_1_galileo.jpg'
-      },
-      {
-        email: 'esteban@hotmail.com',
-        password: '123456',
-        nombre: 'esteban quiroz',
-        avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/d5c5dd71-44d3-4507-aa1e-0584e808ac26/1764054296009_1_esteban.jpg'
-      }
+
     ],
 
     admin: [
-      {
-        email: 'admin@hotmail.com',
-        password: '123456',
-        nombre: 'admin@hotmail.com',
-        avatar: 'https://tuwlrspqlkpqatnaintx.supabase.co/storage/v1/object/public/avatars/2173bce1-49f3-41f9-b600-990518322df2/1764039766395_1_admin.jpg'
-      },
       {
         email: 'till@hotmail.com',
         password: '123456',
