4268ae07b0ccd59b1bc4ca11921baf6a7dd00492
nachomonllor <nachomonllorc@gmail.com>
2025-12-06 20:11:12 -0300

    formateo SI NO en la funcion de dato dinamico

--- src/app/models/dato-dinamico.model.ts
+++ src/app/models/dato-dinamico.model.ts
@@ -7,10 +7,30 @@ export interface DatoDinamico {
   unidad?: string | null;
 }
 
-// función helper ya usada en MiPerfil
+// // función helper ya usada en MiPerfil
+// export function formatearDatoDinamico(d: DatoDinamico): string {
+//   const base = `${d.clave}: ${d.valor ?? ''}`;
+//   if (d.unidad) return `${base} ${d.unidad}`;
+//   return base;
+// }
+
+
+// src/app/models/dato-dinamico.model.ts
+
 export function formatearDatoDinamico(d: DatoDinamico): string {
-  const base = `${d.clave}: ${d.valor ?? ''}`;
-  if (d.unidad) return `${base} ${d.unidad}`;
+  let val = d.valor;
+
+  // Si el valor es booleano (true-false), lo convertimos a texto
+  if (typeof val === 'boolean') {
+    val = val ? 'SI' : 'NO';
+  }
+  // -------------------
+
+  const base = `${d.clave}: ${val ?? ''}`;
+  
+  if (d.unidad) {
+    return `${base} ${d.unidad}`;
+  }
+  
   return base;
 }
-
